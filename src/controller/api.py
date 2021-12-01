2b19be7239501f701b71f8408d6751f1c486508d
韦铜攀 <7845472+wei_tongpan@user.noreply.gitee.com>
2021-12-02 00:02:27 +0800

    '添加关键字'

--- src/controller/api.py
+++ src/controller/api.py
@@ -1,5 +1,6 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint, render_template, request, jsonify
 from src.service.test_service import TestService
+from src.db import DB
 
 api = Blueprint("api", __name__, url_prefix='')
 
@@ -8,16 +9,25 @@ api = Blueprint("api", __name__, url_prefix='')
 def test_hello():
     return '<h1>hello</h1>'
 
+
 # 测试页面  
 @api.route('/index')
 def hello():
-    name = request.args.get("name")
-    result = TestService().test()
-    return render_template("test.html",name=name,result=result)
+    # name = request.args.get("name")
+    # result = TestService().test()
+    return render_template("index.html")
 
 
-@api.route('/subKeys')
-def subKeys():
-    pass
+@api.route('/subKeys',  methods=['post'])
+def sub_keys():
+    key = request.form["key"]
+    name = request.form["name"]
+    number = request.form["number"]
+    text = request.form["text"]
+    if not all([key, name, number, text]):
+        return jsonify(code=400, message="missing parameter")
+    sql = 'INSERT INTO keys ( key, name, number, answer ) VALUES( key, name, number, text )'
+    DB().save()
+    return jsonify(code=200, message="ok!")
 
 
